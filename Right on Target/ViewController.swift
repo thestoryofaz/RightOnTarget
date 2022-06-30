9bfa7c2143c75652a21874a1a531d89371d81583
aiv <96691834+aiv26@users.noreply.github.com>
2022-06-30 21:50:50 +0300

    Storyboard and Logic

--- Right on Target/ViewController.swift
+++ Right on Target/ViewController.swift
@@ -1,19 +1,50 @@
-//
-//  ViewController.swift
-//  Right on Target
-//
-//  Created by aiv on 30.06.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var label: UILabel!
+    
+    var number: Int = 0
+    var round: Int = 0
+    var points: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func checkNumber() {
+        
+        if round == 0 {
+            number = Int.random(in: 1...50)
+            label.text = String(number)
+            round = 1
+        } else {
+            let numSlider = Int(slider.value.rounded())
+            if numSlider > number {
+                points += 50 - numSlider + number
+            } else if numSlider < number {
+                points += 50 - number + numSlider
+            } else {
+                points += 50
+            }
+            if round == 5 {
+                let alert = UIAlertController(title: "Игра окончена",
+                                              message: "Вы заработали \(points) очков",
+                                              preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Начать заново",
+                                              style: .default,
+                                              handler: nil))
+                present(alert, animated: true, completion: nil)
+                round = 1
+                points = 0
+            } else {
+                round += 1
+            }
+            number = Int.random(in: 1...50)
+            label.text = String(number)
+        }
+    }
 }
 
