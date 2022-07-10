76c13dfc25fa50b11215500540682422c07383ab
aiv <96691834+aiv26@users.noreply.github.com>
2022-07-10 17:41:21 +0300

    lifecycle ViewController

--- Right on Target/ViewController.swift
+++ Right on Target/ViewController.swift
@@ -6,45 +6,70 @@ class ViewController: UIViewController {
     @IBOutlet var label: UILabel!
     
     var number: Int = 0
-    var round: Int = 0
+    var round: Int = 1
     var points: Int = 0
+    
+    override func loadView() {
+        super.loadView()
+        print("loadView")
+        
+        let vesionLabel = UILabel(frame: CGRect(x: 20, y: 10, width: 200, height: 20))
+        vesionLabel.text = "Версия 1.1"
+        view.addSubview(vesionLabel)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print("viewDidLoad")
+        number = Int.random(in: 1...50)
+        label.text = String(number)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDissapear")
     }
     
     @IBAction func checkNumber() {
-        
-        if round == 0 {
-            number = Int.random(in: 1...50)
-            label.text = String(number)
-            round = 1
+        let numSlider = Int(slider.value)
+        if numSlider > number {
+            points += 50 - numSlider + number
+        } else if numSlider < number {
+            points += 50 - number + numSlider
         } else {
-            let numSlider = Int(slider.value.rounded())
-            if numSlider > number {
-                points += 50 - numSlider + number
-            } else if numSlider < number {
-                points += 50 - number + numSlider
-            } else {
-                points += 50
-            }
-            if round == 5 {
-                let alert = UIAlertController(title: "Игра окончена",
-                                              message: "Вы заработали \(points) очков",
-                                              preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Начать заново",
-                                              style: .default,
-                                              handler: nil))
-                present(alert, animated: true, completion: nil)
-                round = 1
-                points = 0
-            } else {
-                round += 1
-            }
-            number = Int.random(in: 1...50)
-            label.text = String(number)
+            points += 50
         }
+        if round == 5 {
+            let alert = UIAlertController(title: "Игра окончена",
+                                          message: "Вы заработали \(points) очков",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Начать заново",
+                                          style: .default,
+                                          handler: nil))
+            present(alert, animated: true, completion: nil)
+            round = 1
+            points = 0
+        } else {
+            round += 1
+        }
+        number = Int.random(in: 1...50)
+        label.text = String(number)
     }
 }
+
 
